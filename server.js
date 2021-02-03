d9dcefd4c64bc7027c3de5353ac88608c74b80b9
tekthree <tekthree@gmail.com>
2021-02-02 19:35:53 -0800

    first works from tek

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('ejs');
+// require('ejs');
 const superagent = require('superagent');
 const pg = require('pg');
 
