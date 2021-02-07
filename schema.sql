f3b04b886878b09afcf93dff939c1fe0cca30f20
RYAN <RTIPPER44@GMAIL.COM>
2021-02-06 18:28:52 -0800

    updates to favoites, sql tables, nav bars, bio and button to results

--- schema.sql
+++ schema.sql
@@ -2,14 +2,20 @@ DROP TABLE IF EXISTS hiking;
 CREATE TABLE hiking (
   id SERIAL PRIMARY KEY,
   name VARCHAR(350),
-  rating VARCHAR(20),
+  types VARCHAR(350),
+  business_status VARCHAR(350),
+  formatted_address VARCHAR(350),
+  rating VARCHAR(20)
 );
 
 DROP TABLE IF EXISTS camping;
 CREATE TABLE camping (
   id SERIAL PRIMARY KEY,
   name VARCHAR(350),
-  rating VARCHAR(20),
+  types VARCHAR(350),
+  business_status VARCHAR(350),
+  formatted_address VARCHAR(350),
+  rating VARCHAR(20)
 );
 
 /* database name = blazingdata */
