fd7fc0cab79e97a7a235d7ed03d77398c5ccb8ac
tekthree <tekthree@gmail.com>
2021-02-08 23:34:02 -0800

    pushing bug fixes

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 'use strict';
+
+
+
 //Load environment variables from the .env file
 require('dotenv').config();
 // Step 1:  Bring in our modules/dependencies
@@ -8,30 +11,24 @@ const cors = require('cors');
 const superagent = require('superagent');
 const pg = require('pg');
 const methodOverride = require('method-override');
-
 // Database Connection Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => { throw err; });
-
 // Step 2:  Set up our application
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
 // declare port for server
 const PORT = process.env.PORT || 3000;
-
 //routes
 app.get('/', homeHandler);
+app.get('/pick', searchpageHandler);
+app.get('/favs', favHandler);
 app.post('/search', GHandler);//posting new information to server/search route
-
 //DB Routes
-app.get('/hiking/:hiking_id', getHiking);
-app.get('/camping/:camping_id', getCamping);
-app.post('/results', )
-
+app.post('/saves', saveHikeAndCamp);
 // 
 // function homeHandler(req, res) {
 //   const SQL = 'SELECT * FROM shelf;';
@@ -41,106 +38,62 @@ app.post('/results', )
 //       res.render('pages/index', { book: results.rows });
 //     });
 // }
-function defaultHandler(req, res) {
-  let SQL = 'SELECT * FROM camping, hiking';
-  client.query(SQL)
+
+
+//Home Handler
+function homeHandler(request, response) {
+  response.render('index');
+}
+
+function searchpageHandler(req,res){
+  res.render('pages/pick/tbd');
+}
+
+function favHandler(req, res) {
+  const SQL = 'SELECT * FROM hiking;';
+  return client.query(SQL)
     .then(results => {
+      console.log(results.rows);
       let yourFavs = results.rows;
-      res.status(200).render('pages/index', { data: yourFavs });
+      res.status(200).render('pages/favorites/favorites', { data: yourFavs });
     })
     .catch(err => {
       console.log(err);
     });
 }
 
-function addHiking(request, response) {
-  const {name, types, business_status, formatted_address, rating } = request.body;
-  const sql = 'INSERT INTO hiking (name, types, business_status, formatted_address, rating) VALUES ($1,$2,$3,$4,$5);';
-  const safeValues = [name, types, business_status, formatted_address, rating];
-  client.query(sql, safeValues)
-    .then(() => {
-      console.log(`${name} added to your favorites!`);
-      response.redirect(`pages/searches/search`);
+
+function saveHikeAndCamp(req, response) {
+  const SQL = `INSERT INTO hiking(name,address,status,types,rating)VALUES($1,$2,$3,$4,$5)RETURNING*`;
+  const values = [req.body.name, req.body.address, req.body.status, req.body.types, req.body.rating];
+  console.log(SQL,values);
+  client.query(SQL, values)
+    .then(results => {
+      
     }).catch((error) => {
       console.log(error);
       response.render('pages/error');
     });
 }
 
-function addCamping(request, response) {
-  const { name, types, business_status, formatted_address, rating} = request.body;
-  const sql = 'INSERT INTO camping (name, types, business_status, formatted_address, rating) VALUES ($1,$2,$3,$4,$5);';
-  const safeValues = [name, types, business_status, formatted_address, rating];
-  client.query(sql, safeValues)
-    .then(() => {
-      console.log(`${name} added to your favorites!`);
-      response.redirect(`pages/searches/search`);
-    }).catch((error) => {
-      console.log(error);
-      response.render('pages/error');
-    });
-}
 
-function getHiking(request, response) {
-  const sql = 'SELECT * FROM hiking;';
-   client.query(sql)
-    .then(results => {
-      console.log(results.rows);
-      let data = results.rows;
-      response.status(200).render('pages/favorites/favorites', {data});
-    })
-    .catch((error) => {
-      console.log(error);
-      response.render('pages/error');
-    });
-
-  
-function getCamping(request, response) {
-  const sql = 'SELECT * FROM camping;';
-   client.query(sql)
-    .then(results => {
-      console.log(results.rows);
-      let data = results.rows;
-      response.status(200).render('pages/favorites/favorites', {data});
-    })
-    .catch((error) => {
-      console.log(error);
-      response.render('pages/error');
-    });
-}
 
 // IQAIR API 
 function iHandler(request, response, campArray, weatherData) {
   let location = request.body.city;
   const key = process.env.IQKey;
   let URL = `http://api.airvisual.com/v2/city?city=${location}&state=Washington&country=USA&key=${key}`;
-  console.log(URL)
+  // console.log(URL)
   superagent.get(URL)
     .then(data => {
       const airQ = data.body.data.current.pollution;
-
-      console.log(airQ)
-
       const yourAir = new Quality(airQ);
       response.render('pages/results/results-info', { request, response, campArray, weatherData, yourAir });
+      // console.log('camp array >>>>>>>>>>>>>>>>>>>>>>>>', campArray);
     });
 }
 
-//Home Handler
-function homeHandler(request, response) {
-  response.render('pages/pick/tbd')
-}
 
-
-    
-
-
-
-//Home Handler
-
-function homeHandler(request, response) {
-  response.render('pages/pick/tbd')
-}
 
 //Weather API
 function weatherHandler(request, response, campArray) {
@@ -150,13 +103,13 @@ function weatherHandler(request, response, campArray) {
   superagent.get(url)
     .then(value => {
       const weatherData = value.body.data.map(current => new Weather(current));
+      // console.log('camp array >>>>>>>>>>>>>>>>>>>>>>>>', campArray);
       iHandler(request, response, campArray, weatherData);//render once for EACH route
     }).catch(error => {
       console.log('ERROR', error);
       response.status(500).send('So sorry, something went wrong.');
     });
 }
-
 //Google API 
 function GHandler(request, response) {
   let location = request.body.city;
@@ -165,8 +118,10 @@ function GHandler(request, response) {
   let URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?key=${key}&query=${travelType}+in+${location}`;
   superagent.get(URL)
     .then(data => {
+      console.log(data.body);
       const campArray = data.body.results.map(campGround => new Google(campGround));
       weatherHandler(request, response, campArray);
+      console.log('camp array >>>>>>>>>>>>>>>>>>>>>>>>', campArray);
     });
 }
 //Google Constructor
